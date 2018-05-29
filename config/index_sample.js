40cf01121cb1ec1f08db162f926d7e96267aa599
Yakima <cleveryun@163.com>
2018-05-29 15:33:32 +0800

    add support for use as a command line tool

--- config/index_sample.js
+++ config/index_sample.js
@@ -1,6 +1,5 @@
 const domain = {
-  server1: 'http://www.example.com/',
-  server2: 'http:localhost:6060'
+  server1: 'http:localhost:6060'
 }
 
 const config = {
@@ -10,23 +9,18 @@ const config = {
   root: '/test',
   // 代理请求，将请求转发至其他服务器，然后返回相应的内容
   proxyTable: {
-    '/blog': {
-      target: domain.server2,
-      changeOrigin: true
-    },
-    '/wechat/KqPay': {
+    '/apis/proxy': {
       target: domain.server1,
       changeOrigin: true
     }
   },
   // 读取固定的JSON文件内容作为返回值
   jsonTable: [
-    '/manage2/intention/list',
-    '/manage2/carInsurance/queryDetails'
+    '/apis/json',
   ],
   // 读取用户自定义的内容，可以在此处使用第三方数据模拟工具（默认已经预装了mockjs模块，开箱即用）
   customTable: [
-    '/great/what'
+    '/apis/custom'
   ]
 }
 
