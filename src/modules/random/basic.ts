ec244f809494b9ae6a80efdc2a35d869fc1415fc
Yakima <cleveryun@163.com>
2019-03-22 00:55:58 +0800

    :construction: add Mock.Random.str API (Mock.Random.string as an alias)

--- src/modules/random/basic.ts
+++ src/modules/random/basic.ts
@@ -47,7 +47,7 @@ const natural = (min?: number, max?: number): number => {
  * randomly return a character
  * @param {string} clue 'lower', 'upper', 'number', 'symbol', 'alpha', or other given string
  */
-const char = (clue: string): string => {
+const char = (clue?: string): string => {
   const lower = 'abcdefghijklmnopqrstuvwxyz'
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const number = '0123456789'
@@ -87,6 +87,60 @@ const float = (min?: number, max?: number, minDecimalLength?: number, maxDecimal
   return parseFloat(returnNum)
 }
 
+/**
+ * randomly return a string
+ * @param {string | number} [pool] given string, or length of string
+ * @param {number} [min] length of string, or minimum length of string
+ * @param {number} [max] maximum length of string
+ *
+ * @example
+ * ```javascript
+ * Mock.Random.str('abc', 1, 3) // 'bc', 'a', 'abc', 'aca'
+ *
+ * Mock.Random.str('abd', 1) // 'a', 'b', 'd'
+ *
+ * Mock.Random.str(1, 4) // 'abcd', '4d', '@!A4'
+ *
+ * Mock.Random.str(1) // 'a', '$', '4'
+ *
+ * Mock.Random.str('d') // would always be 'd', not a random result actually ^_^
+ *
+ * Mock.Random.str() // would randomly return a strong of length between 3 and 7, e.g., 'abc78', '78#', '1@3wrAS'
+ * ```
+ */
+const str = (pool?: string | number, min?: number, max?: number): string => {
+  let len
+  if (isDefined(max)) {
+    len = natural(min, max)
+  } else if (isDefined(min)) {
+    if (typeof pool === 'string') {
+      // (pool, length) return specific length of string from given pool
+      len = min
+    } else if (typeof pool === 'number') {
+      // (min, max) return random number (between min and max) of string
+      len = natural(pool, min)
+      pool = void 0
+    }
+  } else if (isDefined(pool)) {
+    if (typeof pool === 'number') {
+      // (length)
+      len = pool
+      pool = void 0
+    } else if (typeof pool === 'string') {
+      len = natural(3, 7)
+    }
+  } else {
+    len = natural(3, 7)
+  }
+  let text = ''
+  if (typeof pool === 'string') {
+    for (let i = 0; i < len; i++) {
+      text += char(pool)
+    }
+  }
+  return text
+}
+
 export default {
   bool,
   boolean: bool,
@@ -96,4 +150,6 @@ export default {
   char,
   character: char,
   float,
+  str,
+  string: str,
 }
